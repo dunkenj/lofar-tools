2e320817c7093ca119e7c34388e3fbdb39aaeda3
dunkenj <ken@ngm21.plus.com>
2016-03-29 22:43:54 +0200

    Scale function and integration of CCDData objects

--- lofar-tools/cutouts.py
+++ lofar-tools/cutouts.py
@@ -1,200 +1,206 @@
+import os
 import numpy as np
-import pandas as pd
-import scipy.optimize as opt
-from scipy.special import erf
+from scipy.special import erfinv
+import matplotlib.pyplot as plt
+import astropy.units as u
+from astropy.io import fits
 
-__all__ = ["Model", "Fit", "opt_err_func", "transform_data", "cumgauss"]
+from astropy.coordinates import SkyCoord
+from astropy.stats import median_absolute_deviation, sigma_clipped_stats
+import astropy.visualization as vis 
+from astroquery.ibe import IbeClass
+
+from ccdproc import CCDData, wcs_project
+
+import urllib
+import urlparse
 
 
-def transform_data(data):
+def load_image(filename):
+    try:
+        data = CCDData.read(filename)
+    except(ValueError):
+        try:
+            data = CCDData.read(filename, unit = u.dyn)
+        except:
+            raise
+    return data
+
+def split_url(url):
     """
-    Function that takes experimental data and gives us the
-    dependent/independent variables for analysis
-
-    Parameters
-    ----------
-    data : Pandas DataFrame or string.
-        If this is a DataFrame, it should have the columns `contrast1` and
-        `answer` from which the dependent and independent variables will be
-        extracted. If this is a string, it should be the full path to a csv
-        file that contains data that can be read into a DataFrame with this
-        specification.
-
+    Extract filename from full url of cutout image
+    
+    Paramters
+    ---------
+    url : string
+        url from which to extract filename
+        
     Returns
     -------
-    x : array
-        The unique contrast differences.
-    y : array
-        The proportion of '2' answers in each contrast difference
-    n : array
-        The number of trials in each x,y condition
+    filename : string
+        extracted filename
+    
     """
-    if isinstance(data, str):
-        data = pd.read_csv(data)
-
-    contrast1 = data['contrast1']
-    answers = data['answer']
-
-    x = np.unique(contrast1)
-    y = []
-    n = []
-
-    for c in x:
-        idx = np.where(contrast1 == c)
-        n.append(float(len(idx[0])))
-        answer1 = len(np.where(answers[idx[0]] == 1)[0])
-        y.append(answer1 / n[-1])
-    return x, y, n
-
-
-def cumgauss(x, mu, sigma):
+    url_split = urlparse.urlsplit(url)
+    head, filename = os.path.split(url_split.path)
+    return filename
+    
+def get_wise(coordinates, ang_size, band=1, clobber=True):
     """
-    The cumulative Gaussian at x, for the distribution with mean mu and
-    standard deviation sigma.
-
+    Get cutouts of ALLWISE images
+        
+    
     Parameters
     ----------
-    x : float or array
-       The values of x over which to evaluate the cumulative Gaussian function
-
-    mu : float
-       The mean parameter. Determines the x value at which the y value is 0.5
-
-    sigma : float
-       The variance parameter. Determines the slope of the curve at the point
-       of Deflection
-
+    coordinates : astropy.coordinates object
+        Central coordinates of desired cutout
+        
+    ang_size : astropy.quantity object
+        Angular diamater of desired cutout (max = 5 degrees)
+    
+    band : integer, default = 1
+        WISE band to get cutout for. WISE filters correspond to rest-frame
+        central wavelengths of W1 = 3.4um, W2 = 4.6um, W3 = 12.0um and 
+        W4 = 22.0um.
+        
+    clobber : bool, default = True
+        Overwrite existing filenames when generating cutouts
+        
     Returns
     -------
-
-    g : float or array
-        The cumulative gaussian with mean $\\mu$ and variance $\\sigma$
-        evaluated at all points in `x`.
-
-    Notes
-    -----
-    Based on:
-    http://en.wikipedia.org/wiki/Normal_distribution#Cumulative_distribution_function
-
-    The cumulative Gaussian function is defined as:
-
-    .. math::
-
-        \\Phi(x) = \\frac{1}{2} [1 + erf(\\frac{x}{\\sqrt{2}})]
-
-    Where, $erf$, the error function is defined as:
-
-    .. math::
-
-        erf(x) = \\frac{1}{\\sqrt{\\pi}} \int_{-x}^{x} e^{t^2} dt
-
+    
+        
     """
-    return 0.5 * (1 + erf((x - mu) / (np.sqrt(2) * sigma)))
+    
+    pixel_scale= 1.3333333333*u.arcsec
+    size_pix = int(ang_size/pixel_scale)
+    
+    Ibe = IbeClass()
+    out = Ibe.query_region_sia(coordinates, mission='wise', 
+                               dataset='allwise', 
+                               table='p3am_cdd', width = ang_size,
+                               intersect='CENTER',
+                               most_centered=True)
+                           
+    query_string ='?center={0},{1}'\
+                  'deg&size={2}arcsec'.format(coordinates.ra.to(u.deg).value,
+                                           coordinates.dec.to(u.deg).value,
+                                           ang_size.to(u.arcsec).value)
+
+    obj_url = out['sia_url'][band-1]
+    filename = split_url(obj_url)
+    if os.path.isfile(filename) and clobber:
+        os.remove(filename)
+        
+    test = urllib.urlretrieve(obj_url+query_string,
+                              filename=filename)
+    print(obj_url+query_string)
+    
+    image = load_image(filename)
+        
+    mad = median_absolute_deviation(image)
+    sigma = mad#/np.sqrt(2)*erfinv(2*0.75-1)
+
+    image = image.subtract(np.median(image)*image.unit)
+    image = image.divide(sigma)
+
+    return image #, out
+
+def get_decals(coordinates, size, band='g', 
+               pix_scale=0.262*u.arcsec, clobber=True):
+               
+    size_pix = np.round((size / pix_scale),0)+1
+    if size_pix > 512:
+        raise Exception()
+    
+    base_url='http://legacysurvey.org/viewer/fits-cutout-decals-dr2?'
+    query_string = 'ra={0}&dec={1}&pixscale={2}'\
+                   '&size={3:.0f}&bands={4}'.format(
+                                              coordinates.ra.to(u.deg).value,
+                                              coordinates.dec.to(u.deg).value,
+                                              pix_scale.to(u.arcsec).value,
+                                              size_pix,
+                                              band)
+
+    filename = 'test.fits'
+    if os.path.isfile(filename) and clobber:
+        os.remove(filename)
+    print base_url+query_string
+    test = urllib.urlretrieve(base_url+query_string,
+                              filename=filename)
+
+    image = load_image(filename)        
+    mad = median_absolute_deviation(image)
+    sigma = mad#/np.sqrt(2)*erfinv(2*0.75-1)
+
+    image = image.subtract(np.median(image)*image.unit)
+    image = image.divide(sigma)
 
 
-def opt_err_func(params, x, y, func):
-    """
-    Error function for fitting a function using non-linear optimization
+    return image #, out
 
+def scale_rgb(red, green, blue, stretch_coeff,
+             pmin=0, pmax=98.):
+    """ Perform interval and stretch for RGB
+    
     Parameters
     ----------
-    params : tuple
-        A tuple with the parameters of `func` according to their order of
-        input
-
-    x : float array
-        An independent variable.
-
-    y : float array
-        The dependent variable.
-
-    func : function
-        A function with inputs: `(x, *params)`
-
+    red : CCDData object
+        Red filter image data, parsed with load_image
+    green : CCDData object
+        Green filter image data, parsed with load_image    
+    blue : CCDData object
+        Blue filter image data, parsed with load_image
+    stretch_coeff : tuple, (1,3)
+        Power-law stretch coefficients for (red, green, blue)
+        filters respectively.
+    pmin : float, default = 0
+        The lower percentile below which to ignore pixels. Passed to 
+        AsymmetricPercentileInterval function.
+    pmax : float, default = 98
+        The upper percentile above which to ignore pixels. Passed to 
+        AsymmetricPercentileInterval function.    
+        
     Returns
     -------
-    float array
-        The marginals of the fit to x/y given the params
+    rgb : np.array, N x N x 3
+    
     """
-    return y - func(x, *params)
+    r_stretch = vis.stretch.PowerStretch(stretch_coeff[0])
+    g_stretch = vis.stretch.PowerStretch(stretch_coeff[1])
+    b_stretch = vis.stretch.PowerStretch(stretch_coeff[2])
+    
+    interval = vis.AsymmetricPercentileInterval(pmin, pmax)
+    
+    rgb = np.array([r_stretch(interval(red)),
+                    g_stretch(interval(green)),
+                    b_stretch(interval(blue))]).T
+    return rgb
 
 
-class Model(object):
-    """ Class for fitting cumulative Gaussian functions to data"""
-    def __init__(self, func=cumgauss):
-        """ Initialize a model object
-
-        Parameters
-        ----------
-        data : Pandas DataFrame
-            Data from a subjective contrast judgement experiment
-
-        func : callable, optional
-            A function that relates x and y through a set of parameters.
-            Default: :func:`cumgauss`
-        """
-        self.func = func
-
-    def fit(self, x, y, initial=[0.5, 1]):
-        """
-        Fit a Model to data
-
-        Parameters
-        ----------
-        x : float or array
-           The independent variable: contrast values presented in the
-           experiment
-        y : float or array
-           The dependent variable
-
-        Returns
-        -------
-        fit : :class:`Fit` instance
-            A :class:`Fit` object that contains the parameters of the model.
-
-        """
-        params, _ = opt.leastsq(opt_err_func, initial,
-                                args=(x, y, self.func))
-        return Fit(self, params)
+asize =  60*u.arcsec
+test_pos = SkyCoord(244.6461*u.deg, 7.4106*u.deg)
+wise = get_wise(test_pos, asize)
+pscale = (wise.header['PXSCAL1']*u.arcsec)
+pscale = 0.5*u.arcsec
 
 
-class Fit(object):
-    """
-    Class for representing a fit of a model to data
-    """
-    def __init__(self, model, params):
-        """
-        Initialize a :class:`Fit` object
+g = get_decals(test_pos, asize, band='g',
+                      pix_scale=pscale)
+r = get_decals(test_pos, asize, band='r',
+                      pix_scale=pscale)
+z = get_decals(test_pos, asize, band='z',
+                      pix_scale=pscale)
 
-        Parameters
-        ----------
-        model : a :class:`Model` instance
-            An object representing the model used
+wise2 = wcs_project(wise, g.wcs, g.shape)
 
-        params : array or list
-            The parameters of the model evaluated for the data
+imstretch = vis.stretch.LogStretch()
 
-        """
-        self.model = model
-        self.params = params
+rgb = scale_rgb(z, r, g, np.array([1, 1, 1])*1., pmin=1, pmax=98)
 
-    def predict(self, x):
-        """
-        Predict values of the dependent variable based on values of the
-        indpendent variable.
+rgb_wise = scale_rgb(wise2, z, g, np.array([0.66, 1, 1])*1., pmin=1, pmax=98)
 
-        Parameters
-        ----------
-        x : float or array
-            Values of the independent variable. Can be values presented in
-            the experiment. For out-of-sample prediction (e.g. in
-            cross-validation), these can be values
-            that were not presented in the experiment.
-
-        Returns
-        -------
-        y : float or array
-            Predicted values of the dependent variable, corresponding to
-            values of the independent variable.
-        """
-        return self.model.func(x, *self.params)
+Fig, Ax = plt.subplots(1,2)
+Ax[0].imshow(rgb, interpolation='nearest')
+Ax[1].imshow(rgb_wise, interpolation='nearest')
+plt.show()
